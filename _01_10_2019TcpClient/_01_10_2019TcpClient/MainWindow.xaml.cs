7062bc10e0d6977e142882ddc7a4eba225d97698
Romanovych <ivankaromanovych12@gmail.com>
2019-10-01 19:52:05 +0300

    Teachers example ans small task done

---
+++ _01_10_2019TcpClient/_01_10_2019TcpClient/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _01_10_2019TcpClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            IPAddress iPAddress = IPAddress.Parse("127.0.0.57");
+            IPEndPoint iPEndPoint = new IPEndPoint(iPAddress, 1098);
+
+            TcpClient client = new TcpClient();
+            client.Connect(iPEndPoint);
+
+            using (NetworkStream stream = client.GetStream())
+            {
+                using (BinaryWriter writer = new BinaryWriter(stream))
+                {
+                    writer.Write("Hi, Telesyk!");
+                    using (BinaryReader reader = new BinaryReader(stream))
+                    {
+                        txtResponse.Text = reader.ReadString();
+                    }
+                }
+            }
+            client.Close();
+        }
+    }
+}
---
+++ _01_10_2019TcpClient/_01_10_2019TcpServer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_10_2019TcpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress iPAddress = IPAddress.Parse("127.0.0.57");
+            IPEndPoint iPEndPoint = new IPEndPoint(iPAddress, 1098);
+            TcpListener tcpListener = new TcpListener(iPEndPoint);
+            tcpListener.Start();
+
+            while(true)
+            {
+                TcpClient client = tcpListener.AcceptTcpClient();
+                using (NetworkStream stream = client.GetStream())
+                {
+                    using (BinaryReader reader = new BinaryReader(stream))
+                    {
+                        Console.WriteLine(reader.ReadString());
+                        using (BinaryWriter writer = new BinaryWriter(stream))
+                        {
+                            writer.Write("Cossack is dead");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
